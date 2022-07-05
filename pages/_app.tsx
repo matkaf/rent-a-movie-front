ab74cb09bcae76b522d5aba7234f3578a19f6a69
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-05 12:31:33 -0300

    feat: configura next e styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/theme'
+import GlobalStyle from '../styles/global'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
